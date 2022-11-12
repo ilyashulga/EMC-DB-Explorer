a6836778c9931d1142f95374ddea03d31e4506da
ilyashulga <shulga.ilya@gmail.com>
2022-11-12 22:28:07 +0200

    Dash "Hello, world"

--- app.py
+++ app.py
@@ -1,1 +1,54 @@
-print("Hello, world")
+
+from sqlite3 import connect
+from dash.dependencies import Input, Output
+from cs50 import SQL
+from dash import Dash, html, dash_table, dcc
+import plotly.express as px
+import pandas as pd
+import os
+
+from helpers import generate_graph, generate_table
+
+
+# Configure standard SQLite3
+db = connect("plotter.db")
+
+# App layout
+app = Dash(__name__, prevent_initial_callbacks=True)
+
+# Read csv content with pandas into dataframe starting from row 18 (otherwise pandas can't read properly the data)
+filename = "All_Traces.csv"
+
+
+df1 = pd.read_csv(os.path.join(filename), skiprows=18)
+
+# Change column names in dataframe to more intuitive
+df1.columns = ['Frequency[MHz]','Max(Ver,Hor)', 'Ver', 'Hor']
+
+fig = generate_graph(df1, "test")
+
+df2 = pd.read_sql("SELECT * FROM graphs JOIN users ON sessions.user_id = users.id JOIN sessions ON graphs.session_id = sessions.id", db)
+
+df2.to_csv('plotter_db.csv')
+print(df2.head())
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    ),
+
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df2, 100)
+])
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ helpers.py
@@ -0,0 +1,35 @@
+import json
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from dash import Dash, html
+
+
+def generate_graph(df, graph_title):
+    """Generate graph in JSON format to be sent to JINJA and embedded inside html"""
+    # Iterate over each row in Dataframe, calculate Max(Column 2, Column 3) or Max(Ver,Hor) on each row and insert into Column 1 or Max(Ver,Hor) column
+    for row in range(len(df)):
+        df.at[row,'Max(Ver,Hor)'] = max(df.at[row,'Hor'], df.at[row,'Ver'])
+        df.at[row,'Frequency[MHz]'] = df.at[row,'Frequency[MHz]']/1000000
+    
+    # create xy chart using plotly library    
+    fig = px.line(df, x='Frequency[MHz]', y='Max(Ver,Hor)', log_x=True, template="plotly_white", title='%s' % graph_title)
+
+    # Convery plotly fig to JSON object
+    #graphJSON = json.dumps(fig, cls = plotly.utils.PlotlyJSONEncoder)
+
+    return fig
+
+
+
+def generate_table(dataframe, max_rows):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
